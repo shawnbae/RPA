82bd2df7c04664d3b02284d5a71b3e0ad7bd993d
shawnbae <tngks0315@naver.com>
2021-01-14 16:08:17 +0900

    20210114 update

--- 4_email/1_send.py
+++ 4_email/1_send.py
@@ -1,14 +1,13 @@
+#-*- coding: UTF-8 -*-
+
 import smtplib
+import urllib.parse # 유니코드 에러 해결을 위한 모듈
 from account import *
-
-import sys
-print(sys.getdefaultencoding()) # 유니코드 에러
-
 
 with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
     smtp.ehlo() # 연결이 잘 수립되는지 확인
     smtp.starttls() # 모든 내용이 암호화되어 전송
-    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD) # 로그인
+    smtp.login(urllib.parse.quote(EMAIL_ADDRESS), urllib.parse.quote(EMAIL_PASSWORD)) # 로그인
 
     subject = "test mail" # 메일 제목
     body = "mail body" # 메일 본문
