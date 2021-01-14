4c0bf3bdedb11950fe3f300e7079371b1cb87454
shawnbae <tngks0315@naver.com>
2021-01-14 15:55:24 +0900

    20210114 update

--- 4_email/1_send.py
+++ 4_email/1_send.py
@@ -1,6 +1,10 @@
 import smtplib
 from account import *
 
+import sys
+print(sys.getdefaultencoding()) # 유니코드 에러
+
+
 with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
     smtp.ehlo() # 연결이 잘 수립되는지 확인
     smtp.starttls() # 모든 내용이 암호화되어 전송
