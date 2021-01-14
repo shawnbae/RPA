ce5f284fe789f6ffe3708d4c6528b32413744908
shawnbae <tngks0315@naver.com>
2021-01-14 16:31:41 +0900

    20210114 update

--- 4_email/1_send.py
+++ 4_email/1_send.py
@@ -2,18 +2,17 @@
 
 import smtplib
 import urllib.parse # 유니코드 에러 해결을 위한 모듈
+from email.mime.text import MIMEText
 from account import *
 
 with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
     smtp.ehlo() # 연결이 잘 수립되는지 확인
     smtp.starttls() # 모든 내용이 암호화되어 전송
-    smtp.login(urllib.parse.quote(EMAIL_ADDRESS), urllib.parse.quote(EMAIL_PASSWORD)) # 로그인
+    smtp.login("tngks0315@gmail.com", "hzim iykx xakb eerv") # 로그인
 
-    subject = "test mail" # 메일 제목
-    body = "mail body" # 메일 본문
-
-    msg = f"Subject: {subject}\n{body}"
+    msg = MIMEText('내용 : 본문내용 테스트입니다.')
+    msg['Subject'] = '제목 : 메일 보내기 테스트입니다.'
 
     # 발신자, 수신자, 정해진 형식의 메시지
-    smtp.sendmail(EMAIL_ADDRESS, "nadocoding@gmail.com", msg) 
+    smtp.sendmail("tngks0315@gmail.com", "tngks0315@naver.com", msg) 
     
