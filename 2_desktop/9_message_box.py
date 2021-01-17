7481bcf2c34875596f7acdb01db0ca5ba378c369
shawnbae <tngks0315@naver.com>
2021-01-17 21:16:17 +0900

    20210117 update

--- 2_desktop/9_message_box.py
+++ 2_desktop/9_message_box.py
@@ -7,6 +7,5 @@ import pyautogui
 #result = pyautogui.confirm("계속 진행하시겠습니까?", "확인") # 확인, 취소 버튼
 #print(result)
 # result = pyautogui.prompt("파일명을 무엇으로 하시겠습니까?", "입력") # 사용자 입력
-
 result = pyautogui.password("암호를 입력하세요") # 암호 입력
 print(result)
