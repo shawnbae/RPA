5cae19ac8d1b1cad191193b992884fff5f0237f6
shawnbae <tngks0315@naver.com>
2021-01-28 16:35:52 +0900

    20210128 update

--- 3_web/5_radio_button.py
+++ 3_web/5_radio_button.py
@@ -17,7 +17,7 @@ if elem.is_selected() == False: # 라디오 버튼이 선택되어 있지 않으
 else: # 라디오 버튼이 선택되어 있다면
     print("선택 되어 있으므로 아무것도 안함")
 
-time.sleep(5) # 5초 대기
+time.sleep(5)
 
 # 선택이 안되어 있으면 선택하기
 if elem.is_selected() == False: # 라디오 버튼이 선택되어 있지 않으면
