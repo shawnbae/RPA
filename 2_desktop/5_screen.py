3aeb26a857adeda02ec8ac2de31e92a7c4d01632
shawnbae <tngks0315@naver.com>
2021-02-02 16:58:25 +0900

    20210202 update

--- 2_desktop/5_screen.py
+++ 2_desktop/5_screen.py
@@ -1,7 +1,7 @@
 import pyautogui
 # 스크린 샷 찍기
-# img = pyautogui.screenshot()
-# img.save("screenshot.png") # 파일로 저장
+img = pyautogui.screenshot()
+img.save("screenshot.png") # 파일로 저장
 
 #pyautogui.mouseInfo()
 # 28,18 34,167,242 #22A7F2
