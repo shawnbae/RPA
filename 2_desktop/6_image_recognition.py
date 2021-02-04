de5c18d6796f8a7384db741d9f33a01cf7de4628
shawnbae <tngks0315@naver.com>
2021-02-04 15:09:09 +0900

    20210204 update

--- 2_desktop/6_image_recognition.py
+++ 2_desktop/6_image_recognition.py
@@ -3,8 +3,8 @@ file_menu = pyautogui.locateOnScreen("file_menu.png")
 print(file_menu)
 pyautogui.click(file_menu)
 
-# trash_icon = pyautogui.locateOnScreen("trash_icon.png")
-# pyautogui.moveTo(trash_icon)
+trash_icon = pyautogui.locateOnScreen("trash_icon.png")
+pyautogui.moveTo(trash_icon)
 
 # screen = pyautogui.locateOnScreen("screenshot.png")
 # print(screen)
