1b6c4b935419232c639ff9694dcef65f9e608180
shawnbae <tngks0315@naver.com>
2021-02-17 13:27:04 +0900

    20210217 update

--- 1_excel/10_chart.py
+++ 1_excel/10_chart.py
@@ -5,7 +5,7 @@ ws = wb.active
 from openpyxl.chart import BarChart, Reference, LineChart
 # B2:C11 까지의 데이터를 차트로 생성
 # bar_value = Reference(ws, min_row=2, max_row=11, min_col=2, max_col=3)
-# bar_chart = BarChart() # 차트 종류 설정 (Bar, Line, Pie, ..)
+bar_chart = BarChart() # 차트 종류 설정 (Bar, Line, Pie, ..)
 bar_chart.add_data(bar_value) # 차트 데이터 추가
 ws.add_chart(bar_chart, "E1") # 차트 넣을 위치 정의
 
