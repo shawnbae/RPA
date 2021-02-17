556e9a4c050f17376b1b595a2f935d8b1060a615
shawnbae <tngks0315@naver.com>
2021-02-17 18:21:49 +0900

    20210217 update

--- 1_excel/1_create_file.py
+++ 1_excel/1_create_file.py
@@ -1,6 +1,6 @@
 from openpyxl import Workbook
 wb = Workbook() # 새 워크북 생성
 ws = wb.active # 현재 활성화된 sheet 가져옴
-ws.title = "NadoSheet" # sheet 의 이름을 변경
+ws.title = "NadoSheet" # sheet 의 이름을 할
 wb.save("sample.xlsx") # sample.xlsx라는 이름으로 경로에 저
 wb.close()
