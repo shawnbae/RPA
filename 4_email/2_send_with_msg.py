66d41fd6a232dfdd26eb2ba98d98fc9a1f4faa96
shawnbae <tngks0315@naver.com>
2021-02-23 20:01:40 +0900

    20210223 update

--- 4_email/2_send_with_msg.py
+++ 4_email/2_send_with_msg.py
@@ -8,7 +8,7 @@ msg["From"] = EMAIL_ADDRESS # 보내는 사람
 msg["To"] = "nadocoding@gmail.com" # 받는 사람
 
 # 여러 명에게 메일을 보낼 때
-msg["To"] = "nadocoding@gmail.com, nadocoding@gmail.com"
+msg["To"] = "example@gmail.com, nadocoding@gmail.com"
 to_list = ["nadocoding@gmail.com", "nadocoding@gmail.com"]
 msg["To"] = ", ".join(to_list)
 
