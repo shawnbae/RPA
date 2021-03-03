ab20005ec08880ddd8ac11cedf5e84cd20bd53fd
shawnbae <tngks0315@naver.com>
2021-03-03 13:52:33 +0900

    20210303 updatE

--- 3_web/6_check_box.py
+++ 3_web/6_check_box.py
@@ -2,7 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome('c:/chromedriver.exe')
 browser.maximize_window()
 
 browser.get('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_checkbox')
