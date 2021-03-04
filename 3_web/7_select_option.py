3528f896bda7011bdd42ce2537ed89a6f4c36db9
shawnbae <tngks0315@naver.com>
2021-03-04 10:09:50 +0900

    20210304 update

--- 3_web/7_select_option.py
+++ 3_web/7_select_option.py
@@ -7,7 +7,7 @@ browser.get('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_option')
 browser.switch_to.frame('iframeResult')
 
 # cars 에 해당하는 element 를 찾고, 드롭다운 내부에 있는 4번째 옵션을 선택
-# elem = browser.find_element_by_xpath('//*[@id="cars"]/option[4]')
+elem = browser.find_element_by_xpath('//*[@id="cars"]/option[4]')
 # option[1] : 첫번째 항목 
 # option[2] : 두번째 항목 
 
