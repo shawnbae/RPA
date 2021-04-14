eca2d17dd3b8e0c7465a3c4844a7ca42fa4721e8
shawnbae <tngks0315@naver.com>
2021-04-14 19:05:56 +0900

    20210414 update

---
+++ example_47/.bashrc
@@ -0,0 +1,2 @@
+export PATH="$PATH: /c/ProgramData/Anaconda3: /c/ProgramData/Anaconda3/Scripts"
+alias python="winpty python.exe"
---
+++ example_47/6_euc_to_utf.py
@@ -0,0 +1,22 @@
+"""
+Author : Soohan Bae
+GitHub : https://github.com/shawnbae
+Last Modification : 2021.04.14.
+"""
+import sys
+
+# 파일 이름을 입력받습니다.
+filename = sys.argv[1]
+
+# euc-kr로 인코딩된 파일을 실행합니다.
+in_file = open(filename, encoding="euc-kr")
+
+# utf-8로 저장할 파일을 실행합니다.
+out_file = open("utf8_" + filename, 'w', encoding="utf-8")
+
+content = in_file.read()
+
+out_file.write(content)
+
+in_file.close()
+out_file.close()
