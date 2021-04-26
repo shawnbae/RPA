be49706f18a9daa5b92e09e85ebb4ae1b1c07998
shawnbae <tngks0315@naver.com>
2021-04-26 23:01:28 +0900

    20210426 update

--- example_47/5_11_3_트위터뉴스봇/main.py
+++ example_47/5_11_3_트위터뉴스봇/main.py
@@ -1,10 +1,4 @@
 #-*-coding:euc-kr
-"""
-Author : Byunghyun Ban
-GitHub : https://github.com/needleworm
-Book : 6개월 치 업무를 하루 만에 끝내는 업무 자동화
-Last Modification : 2020.03.02.
-"""
 
 import sys
 import time
